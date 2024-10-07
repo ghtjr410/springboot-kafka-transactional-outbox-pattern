ce9ea1d3dd8ca26a3a56057494f1ba49d5cdc501
ghtjr410 <ghtjr410@gmail.com>
2024-10-07 09:44:04 +0900

    userprofile-service - 초기 프로젝트 생성 - Spring Reactive Web 의존성 주입 (web flux)

---
+++ userprofile-service/src/main/java/com/ghtjr/userprofile/UserprofileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ghtjr.userprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserprofileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserprofileServiceApplication.class, args);
+	}
+
+}
