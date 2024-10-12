bf7cda4728cad5d25ca04b2e77b5d628d75984e5
ghtjr410 <ghtjr410@gmail.com>
2024-10-12 15:39:10 +0900

    post-poller - pom.xml   - Spring Web, JPA, MySQL Driver, Lombok, Kafka 의존성 주입 - application.properties   - server.port를 4041로 설정   - post.poller.topic.name에 'unprocessed-order-events' 주제명 설정   - 데이터베이스 연결 정보 추가 (url, username, password, driver)   - JPA hibernate 설정 추가 (ddl-auto: none, update)   - Kafka 브로커 설정 추가 (localhost:9092)   - Kafka 프로듀서 설정 (key/value 직렬화 설정)

---
+++ post-poller/src/test/java/com/ghtjr/post_poller/PostPollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghtjr.post_poller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
