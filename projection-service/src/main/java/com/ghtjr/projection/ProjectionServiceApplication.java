647ebd3336b1abd80888402fa5058c2cc4d936bb
ghtjr410 <ghtjr410@gmail.com>
2024-10-13 13:21:28 +0900

    projection-service - 초기 프로젝트 생성 - pom.xml   - Spring Boot, JPA, Kafka, MySQL, Lombok 등 필요한 의존성 추가 - application.properties   - 서버 포트 4042로 설정   - Kafka 설정 추가 (bootstrap-servers 및 토픽, 그룹 ID 설정).   - MySQL 데이터베이스 설정 추가 (JDBC URL, 사용자명, 패스워드, 드라이버 클래스 등 설정)

---
+++ projection-service/src/main/java/com/ghtjr/projection/ProjectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ghtjr.projection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectionServiceApplication.class, args);
+	}
+
+}
---
+++ projection-service/src/test/java/com/ghtjr/projection/ProjectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghtjr.projection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
